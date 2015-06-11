cc075be9f698cc6d5e6422b6c5ec1b88e8356c30
MARCELO VIEIRA <marcelo.vieira@emc.com>
2015-06-11 19:57:21 -0300

    1st try on random color

--- hello.py
+++ hello.py
@@ -20,6 +20,26 @@ if hit_counter < 1:
 	r.set("hit_counter", 1)
 
 @app.route('/')
+
+def htmlcolor(randcolor):
+	def _chkarg(a):
+		if isinstance(a, int):
+			if a < 0:
+				a = 0
+			elif a > 255:
+				a = 255
+		elif isinstance(a, float):
+			if a < 0.0:
+				a = 0
+			elif a > 1.0:
+				a = 255
+			else:
+				a = int(round(a*255))
+		else:
+			raise ValueError('Arguments must be integers or floats.')
+		return a
+	randcolor = _chkarg(randcolor)
+
 def hello():
 	r.incr("hit_counter")
 	COUNTER = r.get("hit_counter")
@@ -37,7 +57,7 @@ def hello():
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid,COUNTER)
+	""".format(randcolor,my_uuid,COUNTER)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
