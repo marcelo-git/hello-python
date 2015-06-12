85a1ad92f15b063eca0335139ee817a6243c60eb
MARCELO VIEIRA <marcelo.vieira@emc.com>
2015-06-11 23:59:47 -0300

    random colors now

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import uuid
 import urlparse
 import redis
 import json
+import random
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -16,48 +17,47 @@ credentials = rediscloud_service['credentials']
 r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
 hit_counter = r.get("hit_counter")
 if hit_counter < 1:
-	
+
 	r.set("hit_counter", 1)
 
+def hex_code_colors():
+    a = hex(random.randrange(0,256))
+    b = hex(random.randrange(0,256))
+    c = hex(random.randrange(0,256))
+    a = a[2:]
+    b = b[2:]
+    c = c[2:]
+    if len(a)<2:
+        a = "0" + a
+    if len(b)<2:
+        b = "0" + b
+    if len(c)<2:
+        c = "0" + c
+    z = a + b + c
+    return "#" + z.upper()
+
 @app.route('/')
-
-def htmlcolor(randcolor):
-	def _chkarg(a):
-		if isinstance(a, int):
-			if a < 0:
-				a = 0
-			elif a > 255:
-				a = 255
-		elif isinstance(a, float):
-			if a < 0.0:
-				a = 0
-			elif a > 1.0:
-				a = 255
-			else:
-				a = int(round(a*255))
-		else:
-			raise ValueError('Arguments must be integers or floats.')
-		return a
-	randcolor = _chkarg(randcolor)
-
 def hello():
 	r.incr("hit_counter")
 	COUNTER = r.get("hit_counter")
+
+	fntcolor = hex_code_colors()
+	bkgcolor = hex_code_colors()
 
 	return """
 	<html>
 	<body bgcolor="{}">
 
-	<center><h1><font color="white">Hi, I'm GUID:<br/>
+	<center><h1><font color="{}">Hi, I'm GUID:<br/>
 	{}
 	</center>
-	<center><h1><font color="white">Number of hits:<br/>
+	<center><h1><font color="{}">Number of hits:<br/>
 	{}
 	</center>
 
 	</body>
 	</html>
-	""".format(randcolor,my_uuid,COUNTER)
+	""".format(bkgcolor,fntcolor,my_uuid,fntcolor,COUNTER)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
